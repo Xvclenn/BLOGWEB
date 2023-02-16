3aeb63883c202e941f30f0ea079de5086409f8ca
Xvclenn <benhhslen@gmail.com>
2023-02-17 00:46:07 +0800

    ...

--- src/components/CreateNews/CreateNews.js
+++ src/components/CreateNews/CreateNews.js
@@ -1,18 +1,30 @@
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import "./CreateNews.css";
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import Form from 'react-bootstrap/Form';
+import './CreateNews.css';
 
 export function CreateNews() {
     return (
         <>
-            <h1>Мэдээ Бэлтгэх Талбар</h1>
-            <div id="inputs" className="container">
-                <input></input>
-                <input></input>
-                <input></input>
-                <input></input>
+            <h1 className='m-5'>Мэдээ Бэлтгэх Талбар</h1>
+            <div id='inputs' className='container'>
+                <input
+                    className='form-control'
+                    placeholder='Мэдээний Гарчиг'
+                ></input>
+                <select className='form-select'>
+                    <option>Ангилалгүй</option>
+                    <option value='1'>One</option>
+                    <option value='2'>Two</option>
+                    <option value='3'>Three</option>
+                </select>
+                <input
+                    className='form-control'
+                    placeholder='Мэдээний Бэлтгэгч'
+                ></input>
+                <input className='form-control'></input>
             </div>
-            <div id="ck" className="container">
+            <div id='ck' className='container'>
                 <CKEditor
                     editor={ClassicEditor}
                     onChange={(event, editor) => {
